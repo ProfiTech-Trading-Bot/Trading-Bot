da2a20c24c5cb878af59911830fdd87b3d2ea783
Kitty Cai <32655726+cat-kitty@users.noreply.github.com>
2021-07-31 10:59:14 -0400

    Introductions

---
+++ sentimental_analysis.py
@@ -0,0 +1,2 @@
+#Fahim Ahmed, Hugh Jiang, Richard Yang, Zhi Rong Cai
+#July 31st, 2021 - August 2nd, 2021
--- stock_price.py
+++ stock_price.py
@@ -1,1 +1,2 @@
-#Hello
+#Fahim Ahmed, Hugh Jiang, Richard Yang, Zhi Rong Cai
+#July 31st, 2021 - August 2nd, 2021
