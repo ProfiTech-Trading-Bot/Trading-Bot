af25b794b3b11cebce501c71867dfa65a0bb9357
hugh-jiang <78945990+hugh-jiang@users.noreply.github.com>
2021-07-31 14:02:57 -0400

    Use constants

--- stock_price.py
+++ stock_price.py
@@ -1,15 +1,23 @@
 #Fahim Ahmed, Hugh Jiang, Richard Yang, Zhi Rong Cai
 #July 31st, 2021 - August 2nd, 2021
 
-from api import ALPHA_VANTAGE_API_TOKEN
-import requests
-import pandas as pd
+#Set up the environment
 from alpha_vantage.timeseries import TimeSeries
+from api import ALPHA_VANTAGE_API_TOKEN
+import numpy as np
+import pandas as pd
+import requests
 import time
 
-API_KEY = 'ZSROA6Z2ZC1HD740'
-ts = TimeSeries(key=API_KEY, output_format='pandas')
-data, meta_data = ts.get_intraday(symbol='MSFT', interval = '1min', outputsize = 'full')
+#Constants
+STOCK_SYMBOL = 'MSFT'
 
+#Create TimeSeries object and make API call
+ts = TimeSeries(key = ALPHA_VANTAGE_API_TOKEN, output_format='pandas')
+data, meta_data = ts.get_daily(symbol = STOCK_SYMBOL)
+
+#Import data
 print('\n')
 print(data)
+print(meta_data)
+
