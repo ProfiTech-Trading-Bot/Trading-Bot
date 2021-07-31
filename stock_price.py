3b772b182cf9f189853bfe012f10d8125ae6951d
hugh-jiang <78945990+hugh-jiang@users.noreply.github.com>
2021-07-31 13:17:24 -0400

    Add API integration with alphavantage

--- stock_price.py
+++ stock_price.py
@@ -2,3 +2,14 @@
 #July 31st, 2021 - August 2nd, 2021
 
 from api import ALPHA_VANTAGE_API_TOKEN
+import requests
+import pandas as pd
+from alpha_vantage.timeseries import TimeSeries
+import time
+
+API_KEY = 'ZSROA6Z2ZC1HD740'
+ts = TimeSeries(key=API_KEY, output_format='pandas')
+data, meta_data = ts.get_intraday(symbol='MSFT', interval = '1min', outputsize = 'full')
+
+print('\n')
+print(data)
