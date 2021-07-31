8e4a1562ccd72f0bbe8aa42a771da30e355e733f
hugh-jiang <78945990+hugh-jiang@users.noreply.github.com>
2021-07-31 16:08:02 -0400

    Add moving averages analysis of price trend

--- stock_price.py
+++ stock_price.py
@@ -3,21 +3,40 @@
 
 #Set up the environment
 from alpha_vantage.timeseries import TimeSeries
-from api import ALPHA_VANTAGE_API_TOKEN
+from api import ALPHA_VANTAGE_API_TOKEN, STOCK_SYMBOL
 import numpy as np
 import pandas as pd
 import requests
 import time
 
-#Constants
-STOCK_SYMBOL = 'MSFT'
-
 #Create TimeSeries object and make API call
 ts = TimeSeries(key = ALPHA_VANTAGE_API_TOKEN, output_format='pandas')
-data, meta_data = ts.get_daily(symbol = STOCK_SYMBOL)
+data, meta_data = ts.get_intraday(symbol= STOCK_SYMBOL, interval ='5min', outputsize='full')
+
 
 #Import data
 print('\n')
-print(data)
-print(meta_data)
 
+# reverse dataframe so most recent date is at the bottom
+data = data.iloc[::-1]
+
+# Get moving averages
+short_ma_period = 9
+short_ma_name = 'MA_' + str(short_ma_period)
+data[short_ma_name] = data.iloc[:,3].rolling(window=short_ma_period).mean()
+
+
+long_ma_period = 21
+long_ma_name = 'MA_' + str(long_ma_period)
+data[long_ma_name] = data.iloc[:,3].rolling(window=long_ma_period).mean()
+ 
+print(data.tail())
+# If shorter period MA is higher than longer period, it means the price is trending upwards
+if data.iloc[-1][short_ma_name] > data.iloc[-1][long_ma_name]:
+    print('Upward Trend')
+    
+elif data.ilc[-1][short_ma_name] < data.iloc[-1][long_ma_name]:
+    print('Downward Trend')
+
+else:
+    print('Stationary Trend')
