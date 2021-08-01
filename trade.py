b1f5bb8740e0cb60ab96cd5281184a830fd8ed64
fahimahmed0x <cpcheats1991@gmail.com>
2021-08-01 11:39:14 -0400

    Complete function to pull the recent price of a stock

--- trade.py
+++ trade.py
@@ -1,6 +1,13 @@
-def getStockPrice(ticker):
-    return
+import pandas as pd
+from alpha_vantage.timeseries import TimeSeries
+from api import ALPHA_VANTAGE_API_TOKEN
 
+def getStockPrice(ticker):
+    #Create TimeSeries object and make API call
+    ts = TimeSeries(key = ALPHA_VANTAGE_API_TOKEN, output_format='pandas')
+    data, meta_data = ts.get_intraday(symbol=ticker, interval='5min', outputsize='full')
+    recentClose = data.iloc[0, 3] #newest value in the 0th row from the 'close' column
+    return recentClose
 
 def buyStock(ticker, quantity, balance):
     #if the user has enough funds for the trade, buy the stock
@@ -16,3 +23,5 @@ def sellStock(ticker, quantity, balance):
     return balance
 
 balance = 10000 #starting account balance of $10,000
+portfolio = pd.DataFrame(columns=['ticker', 'quantity', 'purchasePrice', 'date'])
+print(getStockPrice('AMD'))
