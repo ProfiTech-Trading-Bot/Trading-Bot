0158e03e692f3e6095513c877028fe2969325a83
fahimahmed0x <cpcheats1991@gmail.com>
2021-08-01 12:04:26 -0400

    Update buyStock function

--- trade.py
+++ trade.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import datetime
 from alpha_vantage.timeseries import TimeSeries
 from api import ALPHA_VANTAGE_API_TOKEN
 
@@ -9,13 +10,21 @@ def getStockPrice(ticker):
     recentClose = data.iloc[0, 3] #newest value in the 0th row from the 'close' column
     return recentClose
 
-def buyStock(ticker, quantity, balance):
+def buyStock(ticker, quantity, portfolio):
+    global balance
+    price = getStockPrice(ticker)
+    date = datetime.datetime.now()
     #if the user has enough funds for the trade, buy the stock
-    if balance - quantity * getStockPrice(ticker) >= 0:
+    if balance - quantity * price >= 0:
         #log the trade and update user's balance
-        return balance - quantity * getStockPrice(ticker)
-    #otherwise leave the balance unchanged
-    return balance
+        trade = pd.DataFrame({'ticker': [ticker],
+                                'quantity': [quantity],
+                                'purchasePrice': [price],
+                                'date': [date]})
+        portfolio = portfolio.append(trade)
+        balance = balance - quantity * getStockPrice(ticker)
+
+    return portfolio
 
 def sellStock(ticker, quantity, balance):
     #if the user has enough shares for the trade, sell the stock
@@ -24,4 +33,9 @@ def sellStock(ticker, quantity, balance):
 
 balance = 10000 #starting account balance of $10,000
 portfolio = pd.DataFrame(columns=['ticker', 'quantity', 'purchasePrice', 'date'])
-print(getStockPrice('AMD'))
+#portfolio2 = {}
+
+portfolio = buyStock('AMD', 10, portfolio)
+
+print(balance)
+print(portfolio.head())
