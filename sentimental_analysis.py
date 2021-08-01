283eb14932d2e19e788fc18c9c0a5e951885123d
hugh-jiang <78945990+hugh-jiang@users.noreply.github.com>
2021-08-01 14:39:49 -0400

    Move sentiment analysis to a TweetAnalyzer class

--- sentimental_analysis.py
+++ sentimental_analysis.py
@@ -7,67 +7,82 @@ import tweepy
 from textblob import TextBlob
 import datetime
 
-def cleanTweet(tweet):
-    final_text = tweet.text.replace('RT', '')
-    #Remove starting username tag
-    if final_text.startswith(' @'):
-        position = final_text.index(':')
-        final_text = final_text[position + 2 :]
-    #Remove remaining username tags
-    if final_text.startswith('@'):
-        position = final_text.index(' ')
-        final_text = final_text[position + 1 :]
-    return final_text
+class TweetAnalyzer:
+    '''
+    TweetAnalyzer class. Use getStockSentiment to get Tweet sentiment for a specific ticker symbol.
+    '''
+    def __init__(self, tweet_amount=200):
+        '''
+        Constructor that sets API keys from twitterkeys.txt
+        '''
 
-def stockSentiment(tweets):
-    polarity = 0
-    total = 0
+        # Max number of tweets to check 
+        self.tweet_amount = tweet_amount
 
-    #Processing
-    for tweet in tweets:
-        final_text = cleanTweet(tweet)
-        
-        #Analyze sentiment of tweet
-        analysis = TextBlob(final_text)
-        tweet_polarity = analysis.polarity
+        # Set API Keys
+        mykeys = open('twitterkeys.txt').read().splitlines()
 
-        # For each tweet, -1.0 <= tweet_polarity <= 1.0. The more negative the polarity, the more negative the sentiment.
-        # The more positive the polarity, the more positive the sentiment
-        polarity += tweet_polarity
-        total += 1
+        api_key = mykeys[0]
+        api_key_secret = mykeys[1]
+        access_token = mykeys[2]
+        access_token_secret = mykeys[3]
 
-    # -1.0 <= avg_polarity <= 1.0, larger magnitudes mean a greater positive or negative sentiment
-    avg_polarity = polarity/total
+        auth_handler = tweepy.OAuthHandler(consumer_key=api_key, consumer_secret=api_key_secret)
+        auth_handler.set_access_token(access_token, access_token_secret)
 
-    return avg_polarity
+        self.api = tweepy.API(auth_handler)
 
-mykeys = open('twitterkeys.txt').read().splitlines()
 
-api_key = mykeys[0]
-api_key_secret = mykeys[1]
-access_token = mykeys[2]
-access_token_secret = mykeys[3]
+    def _cleanTweet(self, tweet):
+        '''
+        Private method used to pre-process tweet text in method getStockSentiment()
+        '''
+        final_text = tweet.text.replace('RT', '')
+        #Remove starting username tag
+        if final_text.startswith(' @'):
+            position = final_text.index(':')
+            final_text = final_text[position + 2 :]
+        #Remove remaining username tags
+        if final_text.startswith('@'):
+            position = final_text.index(' ')
+            final_text = final_text[position + 1 :]
+        return final_text
 
-auth_handler = tweepy.OAuthHandler(consumer_key=api_key, consumer_secret=api_key_secret)
-auth_handler.set_access_token(access_token, access_token_secret)
 
-api = tweepy.API(auth_handler)
+    def getStockSentiment(self, ticker_symbol, start_date=datetime.datetime(2021, 7, 30), end_date=datetime.datetime.now()):
+        '''
+        Returns average polarity (sentiment) of tweets based on parameters provided. 
+        -1.0 <= polarity <= 1.0, where more negative values mean more negative sentiment, more positive means more positive sentiment.
 
-#User inputs the stock to analyze
-search_term = input('Enter a stock ticker (ex. GME): ')
-tweet_amount = 200 #only look at 200 tweets
+        NOTE: The minimum date interval is one day. 
+        '''
+        polarity = 0
+        total = 0
 
-#Filter through the tweets between the two dates (not including time).
-start_date = datetime.datetime(2021, 7, 30).date()
-end_date = datetime.datetime.now().date()
+        start_date = start_date.date()
+        end_date = end_date.date()
 
-tweets = tweepy.Cursor(api.search, q=search_term, lang='en', since=start_date, until=end_date).items(tweet_amount)
+        # Get tweets from API
+        tweets = tweepy.Cursor(self.api.search, q=ticker_symbol, lang='en', since=start_date, until=end_date).items(self.tweet_amount)
 
-polarity = stockSentiment(tweets)
+        #Processing
+        for tweet in tweets:
+            final_text = self._cleanTweet(tweet)
+            
+            #Analyze sentiment of tweet
+            analysis = TextBlob(final_text)
+            tweet_polarity = analysis.polarity
 
-#Output sentiment analysis
-print(f"The average polarity of {search_term} is: {polarity}")
-#print(f"Number of positive tweets: {positive_tweets}")
-#print(f"Number of negative tweets: {negative_tweets}")
-#print(f"Number of neutral tweets: {neutral_tweets}")
+            # For each tweet, -1.0 <= tweet_polarity <= 1.0. The more negative the polarity, the more negative the sentiment.
+            # The more positive the polarity, the more positive the sentiment
+            polarity += tweet_polarity
+            total += 1
+
+        # -1.0 <= avg_polarity <= 1.0, larger magnitudes mean a greater positive or negative sentiment
+        avg_polarity = polarity/total
+
+        return avg_polarity
+
+test = TweetAnalyzer()
+print(test.getStockSentiment('GME', datetime.datetime(2021, 7, 31)))
 
